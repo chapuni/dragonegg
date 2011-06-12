24deceb5b0258b0fc2b0d591c6f678b7da19b666
NAKAMURA Takumi <geek4civic@gmail.com>
2011-06-12 21:59:11 +0900

    wip?

--- dll.c
+++ dll.c
@@ -6,18 +6,20 @@ extern FARPROC _AS[], _AE[];
 extern BOOL WINAPI _cygwin_dll_entry(HINSTANCE h, DWORD r, LPVOID d);
 
 BOOL WINAPI lazymain(HINSTANCE h, DWORD r, LPVOID d) {
-  FARPROC *pp = _AS;
-  const char *p = _SS;
+  if (r == DLL_PROCESS_ATTACH) {
+    FARPROC *pp = _AS;
+    const char *p = _SS;
 
-  while (p < _SE) {
-    FARPROC fp;
-    const char *name = p;
-    p++;
-    while (*p++) /*nil*/;
-    fp = GetProcAddress((HMODULE)0, name);
-    if (!fp) return FALSE;
-    *pp++ = fp;
-    while (p < _SE && *p == 0) p++;
+    while (p < _SE) {
+      FARPROC fp;
+      const char *name = p;
+      p++;
+      while (*p++) /*nil*/;
+      fp = GetProcAddress((HMODULE)0, name);
+      if (!fp) return FALSE;
+      *pp++ = fp;
+      while (p < _SE && *p == 0) p++;
+    }
   }
 
   return _cygwin_dll_entry(h, r, d);
