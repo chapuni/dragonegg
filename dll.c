537173cff95cace0c7532b97a374e3bc8206e1ee
NAKAMURA Takumi <geek4civic@gmail.com>
2011-06-23 07:54:06 +0900

    wip: dll.c reworked

--- dll.c
+++ dll.c
@@ -1,25 +1,30 @@
+#include <inttypes.h>
 #include <windows.h>
 
-extern const char _SS[], _SE[];
-extern FARPROC _AS[], _AE[];
+extern const char _image_base__[];
 
-extern BOOL WINAPI _cygwin_dll_entry(HINSTANCE h, DWORD r, LPVOID d);
+#define A(a) (void *)&_image_base__[a]
 
-BOOL WINAPI lazymain(HINSTANCE h, DWORD r, LPVOID d) {
+WINAPI void *__delayLoadHelper2(uint32_t const *desc, void **p)
+{
+  int i;
+  unsigned const *syms = A(desc[4]);
+  void **iat = A(desc[3]);
+  for (i = 0; syms[i]; i++) {
+    IMAGE_IMPORT_BY_NAME const *sym = A(syms[i]);
+    iat[i] = GetProcAddress(NULL, (char const *)sym->Name);
+  }
+
+  return p ? *p : NULL;
+}
+
+extern WINAPI BOOL _cygwin_dll_entry(HINSTANCE h, DWORD r, LPVOID d);
+extern uint32_t const _DELAY_IMPORT_DESCRIPTOR_libcc1_a[];
+
+WINAPI BOOL lazymain(HINSTANCE h, DWORD r, LPVOID d)
+{
   if (r == DLL_PROCESS_ATTACH) {
-    FARPROC *pp = _AS;
-    const char *p = _SS;
-
-    while (p < _SE) {
-      FARPROC fp;
-      const char *name = p;
-      p++;
-      while (*p++) /*nil*/;
-      fp = GetProcAddress((HMODULE)0, name);
-      if (!fp) return FALSE;
-      *pp++ = fp;
-      while (p < _SE && *p == 0) p++;
-    }
+    __delayLoadHelper2(_DELAY_IMPORT_DESCRIPTOR_libcc1_a, NULL);
   }
 
   return _cygwin_dll_entry(h, r, d);
